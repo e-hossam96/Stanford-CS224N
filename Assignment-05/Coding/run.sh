695d8fe34ba19dedd42a6853f85ec9628f2b41ec
E-Hossam96 <ehh.hossam@gmail.com>
2022-12-26 16:56:06 +0200

    Bash code of part g.

--- Assignment-05/Coding/run.sh
+++ Assignment-05/Coding/run.sh
@@ -41,3 +41,26 @@ python src/run.py evaluate vanilla wiki.txt \
     --reading_params_path vanilla.finetune.params \
     --eval_corpus_path birth_test_inputs.tsv \
     --outputs_path vanilla.pretrain.test.predictions
+
+
+# Pretrain the model
+python src/run.py pretrain synthesizer wiki.txt \
+	--writing_params_path synthesizer.pretrain.params
+
+# Finetune the model
+python src/run.py finetune synthesizer wiki.txt \
+	--reading_params_path synthesizer.pretrain.params \
+	--writing_params_path synthesizer.finetune.params \
+	--finetune_corpus_path birth_places_train.tsv
+
+# Evaluate on the dev set; write to disk
+python src/run.py evaluate synthesizer wiki.txt \
+	--reading_params_path synthesizer.finetune.params \
+	--eval_corpus_path birth_dev.tsv \
+	--outputs_path synthesizer.pretrain.dev.predictions
+
+# Evaluate on the test set; write to disk
+python src/run.py evaluate synthesizer wiki.txt \
+	--reading_params_path synthesizer.finetune.params \
+	--eval_corpus_path birth_test_inputs.tsv \
+	--outputs_path synthesizer.pretrain.test.predictions
